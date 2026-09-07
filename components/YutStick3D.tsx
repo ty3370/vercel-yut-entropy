9702f308cc9f323c3d20f1bdf54f0606918d815d
ty3370 <77711112+ty3370@users.noreply.github.com>
2026-09-07 16:33:02 +0900

    Update YutStick3D.tsx

--- components/YutStick3D.tsx
+++ components/YutStick3D.tsx
@@ -6,61 +6,85 @@ import * as THREE from "three";
 
 interface YutStickProps {
   position: [number, number, number];
-  isFlat: boolean;
+  isFlat: boolean; // true: 배(평평한 면이 위), false: 등(둥근 면이 위)
   isRolling: boolean;
 }
 
 export function YutStick({ position, isFlat, isRolling }: YutStickProps) {
-  const meshRef = useRef<THREE.Group>(null);
+  const groupRef = useRef<THREE.Group>(null);
+
+  // 배(평평한 면)가 위: Z축 180도 회전 (Math.PI)
+  // 등(둥근 면)이 위: 0도
   const targetRotationZ = isFlat ? Math.PI : 0;
 
   useFrame((_, delta) => {
-    if (!meshRef.current) return;
+    if (!groupRef.current) return;
 
     if (isRolling) {
-      meshRef.current.rotation.x += delta * 14;
-      meshRef.current.rotation.y += delta * 10;
-      meshRef.current.rotation.z += delta * 12;
-      meshRef.current.position.y = position[1] + Math.sin(Date.now() * 0.015) * 0.4 + 0.4;
+      // 윷 던질 때 공중으로 튀어오르며 역동적 3축 회전
+      groupRef.current.rotation.x += delta * 14;
+      groupRef.current.rotation.y += delta * 11;
+      groupRef.current.rotation.z += delta * 13;
+      groupRef.current.position.y = position[1] + Math.sin(Date.now() * 0.015) * 0.4 + 0.5;
     } else {
-      meshRef.current.rotation.x = THREE.MathUtils.lerp(meshRef.current.rotation.x, 0, delta * 10);
-      meshRef.current.rotation.y = THREE.MathUtils.lerp(meshRef.current.rotation.y, 0, delta * 10);
-      meshRef.current.rotation.z = THREE.MathUtils.lerp(meshRef.current.rotation.z, targetRotationZ, delta * 10);
-      meshRef.current.position.y = THREE.MathUtils.lerp(meshRef.current.position.y, position[1], delta * 10);
+      // 목표 각도로 부드럽게 안착
+      groupRef.current.rotation.x = THREE.MathUtils.lerp(groupRef.current.rotation.x, 0, delta * 10);
+      groupRef.current.rotation.y = THREE.MathUtils.lerp(groupRef.current.rotation.y, 0, delta * 10);
+      groupRef.current.rotation.z = THREE.MathUtils.lerp(groupRef.current.rotation.z, targetRotationZ, delta * 10);
+      groupRef.current.position.y = THREE.MathUtils.lerp(groupRef.current.position.y, position[1], delta * 10);
     }
   });
 
   return (
     // @ts-ignore
-    <group ref={meshRef} position={position}>
-      {/* @ts-ignore */}
-      <mesh position={[0, 0, 0]}>
+    <group ref={groupRef} position={position}>
+      {/* 
+        윷가락 형태 (가로 길이 2.2, 반지름 0.26):
+        - +Y 방향: 둥근 등 (짙은 나무색)
+        - -Y 방향: 평평한 배 (밝은 나무색)
+      */}
+      <group>
+        {/* 1. 둥근 등 (위로 볼록한 반원 기둥) */}
         {/* @ts-ignore */}
-        <cylinderGeometry args={[0.25, 0.25, 2.2, 16, 1, false, 0, Math.PI]} />
+        <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+          {/* @ts-ignore */}
+          <cylinderGeometry args={[0.26, 0.26, 2.2, 32, 1, false, -Math.PI / 2, Math.PI]} />
+          {/* @ts-ignore */}
+          <meshStandardMaterial color="#683d1e" roughness={0.5} side={THREE.DoubleSide} />
+        </mesh>
+
+        {/* 2. 평평한 배 (뚫려 있던 단면을 완벽히 밀봉하는 사각 상판) */}
         {/* @ts-ignore */}
-        <meshStandardMaterial color="#8B5A2B" roughness={0.4} />
-      </mesh>
-      {/* @ts-ignore */}
-      <mesh position={[0, 0, 0]} rotation={[0, Math.PI / 2, 0]}>
+        <mesh position={[0, -0.015, 0]}>
+          {/* @ts-ignore */}
+          <boxGeometry args={[0.52, 0.03, 2.2]} />
+          {/* @ts-ignore */}
+          <meshStandardMaterial color="#edd4a8" roughness={0.3} />
+        </mesh>
+
+        {/* 3. 배 표면의 전통 윷 표식점 3개 */}
         {/* @ts-ignore */}
-        <boxGeometry args={[0.02, 2.2, 0.5]} />
+        <mesh position={[0, -0.031, 0.55]} rotation={[Math.PI / 2, 0, 0]}>
+          {/* @ts-ignore */}
+          <cylinderGeometry args={[0.045, 0.045, 0.01, 16]} />
+          {/* @ts-ignore */}
+          <meshStandardMaterial color="#30190a" />
+        </mesh>
         {/* @ts-ignore */}
-        <meshStandardMaterial color="#F5DEB3" roughness={0.3} />
-      </mesh>
-      {/* @ts-ignore */}
-      <mesh position={[0.02, 0.5, 0]}>
+        <mesh position={[0, -0.031, 0]} rotation={[Math.PI / 2, 0, 0]}>
+          {/* @ts-ignore */}
+          <cylinderGeometry args={[0.045, 0.045, 0.01, 16]} />
+          {/* @ts-ignore */}
+          <meshStandardMaterial color="#30190a" />
+        </mesh>
         {/* @ts-ignore */}
-        <sphereGeometry args={[0.04, 8, 8]} />
-        {/* @ts-ignore */}
-        <meshStandardMaterial color="#3A200B" />
-      </mesh>
-      {/* @ts-ignore */}
-      <mesh position={[0.02, -0.5, 0]}>
-        {/* @ts-ignore */}
-        <sphereGeometry args={[0.04, 8, 8]} />
-        {/* @ts-ignore */}
-        <meshStandardMaterial color="#3A200B" />
-      </mesh>
+        <mesh position={[0, -0.031, -0.55]} rotation={[Math.PI / 2, 0, 0]}>
+          {/* @ts-ignore */}
+          <cylinderGeometry args={[0.045, 0.045, 0.01, 16]} />
+          {/* @ts-ignore */}
+          <meshStandardMaterial color="#30190a" />
+        </mesh>
+      </group>
     </group>
   );
 }
